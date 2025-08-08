dcc93b0f9952a9ccb6b607b7f123dabd7488db00
Alexander Smirnov <satbee@gmx.com>
2025-08-08 22:46:12 +0500

    fix hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-$gxuz5dhzs^wfi4g@nm$2y*e81hh4r@mmhh1k14ab$*z(j8)!b')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-gxuz5dhzs^wfi4g@nm$2y*e81hh4r@mmhh1k14ab$*z(j8)!b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.25.211', '127.0.0.1', 'localhost', 'taskitest.ddns.net;']
+ALLOWED_HOSTS = ['51.250.25.211', '127.0.0.1', 'localhost', 'taskitest.ddns.net']
 
 
 # Application definition
